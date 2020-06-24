79e2755adf313c7bb8a3cd8da2b4908ca6c074aa
NikolsonAkaSamurai <nik.syu@mail.ru>
2020-06-24 12:54:26 +0300

    Дописал программу на два сустава
    Рабочая

--- C++/Robotic-Arm/Robotic_Arm.cpp
+++ C++/Robotic-Arm/Robotic_Arm.cpp
@@ -1,39 +1,38 @@
-#include <math.h> 
-const int a = 100;//длина плеча
-const int b = 100;//длина локтя
-double radiana = 57.2958; // операции синуса и косинуса требуют угол в радианах
-double c;// расстояние от плеча до кисти
-double alpha;//Угол плеча к плостоксти XY
-double beta;// Угол между плечом и локтем
-double gamma;// Угол между плечом и проекцией локтя
-double delta; //Угол луча AC относительно стола
-double X_axis;//Координата кисти по Х
-double Z_axis;//Координата кисти по Z
+﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+using namespace std; // использование стандартной библиотеки
 
-void setup() {
-  Serial.begin(9600);
+int main(int argc, const char* argv[]) {
 
-}
+	setlocale(LC_ALL, "rus");// Команда чтения русских символов!!!
 
-void loop() {
-  alpha = 69.1;
-  beta = 56.6;
-  gamma = ( 180 - beta );
-  delta = alpha - gamma/2;
-  c = (sin((beta/2)/radiana))*b*2;
-  X_axis = cos(delta)*c;
-  Z_axis = sin(delta)*c;
-  
-  Serial.print("rasstoyanie: ");
-  Serial.print(c);
-  Serial.print("   ");
-  Serial.print("Ugol: ");
-  Serial.print(delta);
-  Serial.print("   ");
-  Serial.print("X_axis:  ");
-  Serial.print(X_axis);
-  Serial.print("   ");
-  Serial.print("Z_axis:  ");
-  Serial.println(Z_axis);
-  delay(1000);
+	const int a = 100;//длина плеча
+	const int b = 100;//длина локтя
+	double radiana = 57.2958; // операции синуса и косинуса требуют угол в радианах
+	float c;// расстояние от плеча до кисти
+	double alpha;//Угол плеча к плостоксти XY
+	double beta;// Угол между плечом и локтем
+	double gamma;// Угол между плечом и проекцией локтя
+	double delta; //Угол луча AC относительно стола
+	double X_axis;//Координата кисти по Х
+	double Z_axis;//Координата кисти по Z
+
+	cout << "Введите угол плеча и нажмите Enter" << endl;
+	cin >> alpha;
+	cout << "Введите угол между плечом и локтем и нажмите Enter" << endl;
+	cin >> beta;
+	gamma = 180 - beta;
+	delta = ( alpha - ( gamma / 2) );
+	c = (sin((beta / 2) / radiana)) * b * 2;
+	X_axis = cos(delta/radiana) * c;
+	Z_axis = sin(delta/radiana) * c;
+	cout << "Угол от плеча до кисти относительно стола" << "  " << delta << endl;
+	cout << "Расстояние от плеча до кисти" << "  " << c << endl;
+	cout << "Координата X" << "  " << X_axis << "  " << "Координата Z" << Z_axis << endl;
+	
+
+
+	cin.get();
+	return 0;
 }
